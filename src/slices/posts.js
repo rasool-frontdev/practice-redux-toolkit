79a8266251eeb940fae83cb0ce955ca799df74a4
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-08-15 18:32:55 +0500

    first commit, deleted unneccessary files

---
+++ src/slices/posts.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "Lorem Ipsum is Lore than others who share Lore",
+  },
+  {
+    id: "2",
+    title: "Slicess",
+    content: "Lorem Ipsum is Lore than others who share Lore",
+  },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+});
