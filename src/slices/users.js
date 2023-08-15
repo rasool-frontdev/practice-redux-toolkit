96096c6e6c951a7565e4011838a9da025be9c8f7
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-08-15 21:07:10 +0500

    added users

--- src/slices/users.js
+++ src/slices/users.js
@@ -1,24 +1,29 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+const initialState = [
+  {
+    id: "0",
+    name: "Rasool",
+  },
+  {
+    id: "1",
+    name: "Dave",
+  },
+];
 
-const USERS_URL = "https://jsonplaceholder.typicode.com/users";
-
-const initialState = [];
-
-export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-  const response = await axios.get(USERS_URL);
-  return response.data;
-});
+// export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+//   const response = await axios.get(USERS_URL);
+//   return response.data;
+// });
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
-  extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      return action.payload;
-    });
-  },
+  // extraReducers(builder) {
+  //   builder.addCase(fetchUsers.fulfilled, (state, action) => {
+  //     return action.payload;
+  //   });
+  // },
 });
 
 export const selectAllUsers = (state) => state.users;
